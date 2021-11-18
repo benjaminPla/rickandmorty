b545059e77e8f5bd686017937d39d4491f878545
benjaminPla <benjaminpla.dev@gmail.com>
2021-11-18 09:12:38 -0300

    add components/CharactersFilter.vue && add some styles

---
+++ src/components/CharactersFilter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+      <button>All</button>
+      <button>Alive</button>
+      <button>Dead</button>
+      <button>Unknown</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CharactersFilter',
+};
+</script>
+
+<style scoped lang='scss'>
+.container {
+    background-color: #555;
+    border-radius: 10px;
+    display: inline-block;
+    margin-bottom: 15px;
+    padding: 10px 15px;
+    button {
+      background: none;
+      border: none;
+      color: #fff;
+      cursor: pointer;
+      margin: 0 10px;
+      outline: none;
+    }
+    button:hover {
+      color: orange;
+    }
+}
+
+</style>
--- src/components/ListCharacters.vue
+++ src/components/ListCharacters.vue
@@ -1,20 +1,24 @@
 <template>
-  <h2>Characters</h2>
-  <ul class="characters">
-    <li v-for="character in characters" :key="character.id">
-      <character-card :character='character' />
-    </li>
-  </ul>
+  <div class="flex">
+    <h2>Characters</h2>
+    <characters-filter  class='filter'/>
+    <ul class="characters">
+      <li v-for="character in characters" :key="character.id">
+        <character-card :character='character' />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import { onMounted, computed } from 'vue';
 import { useStore } from 'vuex';
 import CharacterCard from './CharacterCard.vue';
+import CharactersFilter from './CharactersFilter.vue';
 
 export default {
   name: 'ListCharacters',
-  components: { CharacterCard },
+  components: { CharacterCard, CharactersFilter },
   setup() {
     const store = useStore();
     const characters = computed(() => store.state.characters);
@@ -29,14 +33,20 @@ export default {
 </script>
 
 <style scoped lang="scss">
-h2{
-  text-align: center;
-  color: #fff;
-  padding: 20px 0;
-}
-.characters {
-  display: grid;
-  gap: 20px;
-  grid-template-columns: repeat(3, 1fr);
+.flex {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  h2{
+    color: #fff;
+    padding: 20px 0;
+  }
+  .characters {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(3, 1fr);
+    padding: 15px 0;
+  }
 }
 </style>
