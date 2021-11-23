d5bd345b9a3db700c389ce53b54579e1bbb67d28
Benjamin Pla <benjaminpla.dev@gmail.com>
2021-11-23 10:41:26 -0300

    Update ContactForm.vue

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -5,7 +5,7 @@
       <input type='email' placeholder='Email...' />
       <input type='text' placeholder='Subject...' />
       <textarea placeholder='Message...'></textarea>
-      <input type='submit' value='Send' @click.prevent />
+      <input type='submit' value='Under Construction' @click.prevent />
     </form>
   </div>
 </template>
